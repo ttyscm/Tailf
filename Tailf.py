df15ff68c2c1d156030e3800c2c3f17ca31b7d92
ttyscm <ttyscm@gmail.com>
2016-03-24 00:17:10 +0900

    modify: convert tab to space

--- Tailf.py
+++ Tailf.py
@@ -13,55 +13,55 @@ _path = sublime.load_settings('Tailf.sublime-settings').get('path')
 
 
 def tail_f():
-	global _stop, _path
-	f = open(_path)
-	f.seek(0, 2)
-	while True:
-		if _stop:
-			break
-		pos = f.tell()
-		line = f.readline()
-		if not line:
-			time.sleep(0.5)
-			f.seek(pos)
-		else:
-			print line,
+    global _stop, _path
+    f = open(_path)
+    f.seek(0, 2)
+    while True:
+        if _stop:
+            break
+        pos = f.tell()
+        line = f.readline()
+        if not line:
+            time.sleep(0.5)
+            f.seek(pos)
+        else:
+            print line,
 
-	f.close()
-	_stop = False
+    f.close()
+    _stop = False
 
 
 class TailfStartCommand(sublime_plugin.ApplicationCommand):
 
-	def run(self):
-		global _thread, _path
-		if not _path:
-			sublime.message_dialog('cannot find "path" in Tailf.sublime-settings.')
-			return
+    def run(self):
+        global _thread, _path
+        if not _path:
+            sublime.message_dialog('cannot find "path" in Tailf.sublime-settings.')
+            return
 
-		if _thread is not None and _thread.is_alive():
-			sublime.status_message('thread is already started.')
-			return
+        if _thread is not None and _thread.is_alive():
+            sublime.status_message('thread is already started.')
+            return
 
-		_thread = threading.Thread(target=tail_f)
-		_thread.start()
-		sublime.status_message('thread started.')
+        _thread = threading.Thread(target=tail_f)
+        _thread.start()
+        sublime.status_message('thread started.')
 
 
 class TailfStopCommand(sublime_plugin.ApplicationCommand):
 
-	def run(self):
-		global _thread, _stop
-		if _thread is not None and _thread.is_alive():
-			_stop = True
-			_thread.join()
-			_thread = None
-		sublime.status_message('thread stopped.')
+    def run(self):
+        global _thread, _stop
+        if _thread is not None and _thread.is_alive():
+            _stop = True
+            _thread.join()
+            _thread = None
+        sublime.status_message('thread stopped.')
 
 
 class TailfStatusCommand(sublime_plugin.ApplicationCommand):
 
-	def run(self):
-		global _thread
-		print _thread
-		print sublime.load_settings('Tailf.sublime-settings').get('path')
+    def run(self):
+        global _thread
+        print _thread
+        print sublime.load_settings('Tailf.sublime-settings').get('path')
